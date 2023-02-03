c9be1a0fb8ae3b0c725f130078af49f639c4098e
PabloScata <pscataglini95@gmail.com>
2023-02-03 18:30:44 -0300

    Mejora pre entrega 1

--- PreEntregas/main.js
+++ PreEntregas/main.js
@@ -1,18 +1,16 @@
 let bebida = parseInt(prompt("¿Cuantos Gines Forajido?"));
 
-    for(let i = 5; i<=25; i++){
-    if (bebida <=5){
-        let resultado;
-        function multiplicar(bebida, valor=1400){
-            resultado = bebida * valor;
-        }
+function multiplicar(bebida){
+    return bebida * 1400;
+}
+while (bebida != "ESC"){
+    if (bebida <=25){
+        let resultado=multiplicar(bebida)
         console.log(resultado)
-        let mensaje = `El costo total es $ ${resultado}`;
-    alert(mensaje);
-    } else if (bebida <= 25) {
-            alert ("Tenemos disponibilidad"); 
+        let mensaje = `Su carrito es $ ${resultado}`;
+        alert(mensaje);
     } else {
         alert ("No tenemos stock"); 
     }
-        break;
+    bebida = prompt("¿Cuantos Gines Forajido?");
 }
